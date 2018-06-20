98ec821f2534f4d5e045f6872d8ae75d0a5cf76c
Christofer Chan <devchumbing@gmail.com>
2018-06-20 04:32:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ end
 
   def word_substituter(tweet)
   # replaces long words with their expected short form
-    
+
     tweet.split.map do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
