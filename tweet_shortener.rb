f2d167b698af4301943a7dcf2e4d64f67f522761
Christofer Chan <devchumbing@gmail.com>
2018-06-20 03:28:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def dictionary
+  {
+  hello: "hi",
+  two: "2",
+  too: "2",
+  to: "2",
+  for: "4",
+  four: "4",
+  be: "b",
+  you: "u",
+  at: "@",
+  and: "&"
+  }
+end
