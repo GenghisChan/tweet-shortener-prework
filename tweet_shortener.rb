6f6df07aece8d0fb481a3431e3cf0847cdf9f1b8
Christofer Chan <devchumbing@gmail.com>
2018-06-20 04:14:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,7 +12,7 @@ def dictionary
   and: "&"
   }
 end
- 
+
   def word_substituter(tweet)
   # replaces long words with their expected short form
 
@@ -25,7 +25,6 @@ end
 end.join(" ")
 end
 
-end
 
 def bulk_tweet_shortener(tweet)
   if tweet.length > 140
